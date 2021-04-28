b30c3c9b166f99fb4035ac7e31fee44a84c0fc37
Alex Woods <alexhwoods@gmail.com>
2021-04-28 16:48:40 -0400

    Simplify

--- index.ts
+++ index.ts
@@ -1,18 +1,13 @@
 import * as pulumi from "@pulumi/pulumi"
 import { LocalWorkspace } from "@pulumi/pulumi/automation"
 
-console.log('foo')
-
 async function main() {
-  const temp = await LocalWorkspace.createOrSelectStack({
+  const stack = await LocalWorkspace.createOrSelectStack({
     stackName: 'dev',
     workDir: `${__dirname}/sub-a`
   })
 
-  const x = await temp.up()
-  console.log('completed')
-  console.log(`x.stdout: ${x.stdout}`)
-  console.log(x.outputs)
+  const x = await stack.up()
 }
 
 main()
