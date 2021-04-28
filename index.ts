d2345edfaab492411e5f638e5bb07a80f5c07a1d
Alex Woods <alexhwoods@gmail.com>
2021-04-28 16:38:53 -0400

    Add logs

--- index.ts
+++ index.ts
@@ -1,14 +1,18 @@
 import * as pulumi from "@pulumi/pulumi"
 import { LocalWorkspace } from "@pulumi/pulumi/automation"
 
+console.log('foo')
+
 async function main() {
   const temp = await LocalWorkspace.createOrSelectStack({
     stackName: 'dev',
     workDir: `${__dirname}/sub-a`
   })
 
-
-  await temp.up()
+  const x = await temp.up()
+  console.log('completed')
+  console.log(`x.stdout: ${x.stdout}`)
+  console.log(x.outputs)
 }
 
-export default main()
+main()
