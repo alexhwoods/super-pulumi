7c7dea90bb52a7bf33924e8857d646521f04404c
Alex Woods <alexhwoods@gmail.com>
2021-04-28 17:11:13 -0400

    Working

--- index.ts
+++ index.ts
@@ -2,18 +2,16 @@ import * as pulumi from "@pulumi/pulumi"
 import { LocalWorkspace } from "@pulumi/pulumi/automation"
 
 const main = async () => {
-  console.log('called')
   const stack = await LocalWorkspace.createOrSelectStack({
     stackName: 'dev',
     workDir: `${__dirname}/sub-a`
   })
 
-  console.log('called 2')
-
-  const x = await stack.up()
-  console.log('hello')
-  console.log(x)
-  console.log(`foo: ${x.stdout}`)
+  try {
+    const x = await stack.preview()
+  } catch (err) {
+    console.log(err)
+  }
 }
 
 main()
